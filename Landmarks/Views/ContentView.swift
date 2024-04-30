3cdc449c7afa1c037ecf93d832b867d4c6914023
ccsqQuyenNguyen <ntquyen98@gmail.com>
2024-04-30 14:32:59 +0700

    building list and apply navigation from list item to detail screen

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Quyen Quyen on 30/04/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Quyen Quyen on 30/04/2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack{
+            landmark.image.resizable().frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    Group{
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+    }
+}
